33c7d7f068f8832daa7cbb947fa41eb5909c536b
Naceja <edutoriani13@gmail.com>
2023-05-09 22:09:05 -0300

    Finalizado tela de perfil

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,9 +1,59 @@
-import { Center, Text } from "native-base";
+import { Pressable, ScrollView, VStack, Text, Heading, IScrollViewProps } from "native-base";
+import HeaderScreen from "../components/HeaderScreen";
+import UserAvatar from "../components/UserAvatar";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 export default function Profile() {
     return (
-        <Center>
-            <Text>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <HeaderScreen title="Meu Perfil"/>
+            <ScrollView  _contentContainerStyle={{ p: "5", gap: 5 } as Partial<IScrollViewProps>}>
+                <VStack space="2" alignItems="center" px="24">
+                    <UserAvatar 
+                        source={{ uri: 'https://github.com/MrNaceja.png' }}
+                        alt="Avatar do Usuário"
+                        size="32"
+                    />
+                    <Pressable 
+                        _pressed={{ opacity: .5 }}
+                        px="5"
+                        py="2"
+                        w="full"
+                        rounded="md"
+                        alignItems="center"
+                        borderWidth={2}
+                        borderColor={"indigo.500"}
+                    ><Text color="indigo.500">Trocar foto</Text></Pressable>
+                </VStack>
+                
+                <VStack space="3">
+                    <Input 
+                        placeholder="seu nome"
+                    />
+                    <Input 
+                        placeholder="seu email"
+                        isDisabled
+                    />
+                </VStack>
+
+                <VStack space="3" mt="8">
+                    <Heading color="gray.400" fontSize="lg">Alteração de senha</Heading>
+                    <Input 
+                        placeholder="senha antiga"
+                        secureTextEntry
+                    />
+                    <Input 
+                        placeholder="nova senha"
+                        secureTextEntry
+                    />
+                    <Input 
+                        placeholder="confirmar a nova senha"
+                        secureTextEntry
+                    />
+                </VStack>
+                <Button text="Salvar"/>
+            </ScrollView>
+        </VStack>
     )
 }
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,18 +1,29 @@
-import { IImageProps, Image } from "native-base";
+import { IImageProps, Image, Skeleton } from "native-base";
+import { SizeType } from "native-base/lib/typescript/components/types";
 
-interface UserAvatrProps extends IImageProps {
-    size: number
+interface UserAvatarProps extends IImageProps {
+    size: SizeType,
+    loaded?: boolean
 }
 
-export default function UserAvatar({size, ...imageNativeProps} : UserAvatrProps) {
+export default function UserAvatar({size, loaded = true, ...imageNativeProps} : UserAvatarProps) {
     return (
-        <Image 
+        <Skeleton 
+            isLoaded={loaded}
             h={size}
             w={size}
             rounded="full"
-            borderWidth="2"
-            borderColor="gray.600"
-            {...imageNativeProps}
-        />
+            startColor="gray.700"
+            endColor={"gray.600"}
+        >
+            <Image 
+                h={size}
+                w={size}
+                rounded="full"
+                borderWidth="2"
+                borderColor="gray.600"
+                {...imageNativeProps}
+            />
+        </Skeleton>
     )
 }
