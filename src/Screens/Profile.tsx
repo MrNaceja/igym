7c59740d68ffdf5af0023e87cc5b0e36fc439f4b
Naceja <edutoriani13@gmail.com>
2023-05-10 17:19:02 -0300

    Troca de foto de perfil e alerta toast

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,21 +1,82 @@
-import { Pressable, ScrollView, VStack, Text, Heading, IScrollViewProps } from "native-base";
+import { useState } from "react";
+import { Pressable, ScrollView, VStack, Text, Heading, IScrollViewProps, useToast } from "native-base";
 import HeaderScreen from "../components/HeaderScreen";
 import UserAvatar from "../components/UserAvatar";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import { Alert } from 'react-native'
+
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system';
+
 
 export default function Profile() {
+    const [userAvatarProfile, setUserAvatarProfile] = useState('https://github.com/MrNaceja.png')
+    const [userAvatarLoaded, setUserAvatarLoaded]   = useState(true)
+    const toast = useToast()
+
+    function onPressOpenImageGallery() {
+        setUserAvatarLoaded(false)
+        try {
+            changeUserAvatarImage()
+        } catch (error) {
+            console.log(error)
+        }
+        finally{
+            setUserAvatarLoaded(true)
+        }
+    }
+
+    async function changeUserAvatarImage() {
+        const galleryDataSelect = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+        })
+        
+        if (galleryDataSelect.canceled) {
+            return;
+        }
+        const imageSelected = galleryDataSelect.assets[0].uri
+        if (imageSelected) {
+            const imageSelectedInfo = await FileSystem.getInfoAsync(imageSelected)
+            if (imageSelectedInfo.exists) {
+                const imageSize = imageSelectedInfo.size / 1024 / 1024
+                if (imageSize > 3) {
+                    return toast.show({
+                        title: "Imagem muito grande!",
+                        description: "Por favor selecione uma imagem de até 3MB",
+                        placement: "top",
+                        bg: "red.500",
+                        _title: {
+                            textAlign:"center"
+                        }
+                    })
+                }
+            }
+            setUserAvatarProfile(imageSelected)
+            return toast.show({
+                title: "Imagem alterada com sucesso",
+                placement: "top",
+                bg: "indigo.500"
+            })
+        }
+    }
+
     return (
         <VStack flex={1}>
             <HeaderScreen title="Meu Perfil" description="Visualize e atualize informações do seu perfil"/>
             <ScrollView  _contentContainerStyle={{ p: "5", gap: 5 } as Partial<IScrollViewProps>}>
                 <VStack space="2" alignItems="center" px="24">
                     <UserAvatar 
-                        source={{ uri: 'https://github.com/MrNaceja.png' }}
+                        source={{ uri: userAvatarProfile }}
                         alt="Avatar do Usuário"
                         size="32"
+                        loaded={userAvatarLoaded}
                     />
                     <Pressable 
+                        onPress={onPressOpenImageGallery}
                         _pressed={{ opacity: .5 }}
                         px="5"
                         py="2"
